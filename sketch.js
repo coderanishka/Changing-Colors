80eb82232933b13dfc497c329feff37fdcee1690
coderanishka <62501236+coderanishka@users.noreply.github.com>
2020-04-09 17:23:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,21 +16,24 @@ function setup(){
 
 function draw(){
  
-  
   // change the value of Red based on the mouse movement about the X axis
-  r= map(mouseX,100,400,50,200);
+  r = map(mouseX,100,400,50,150);
+  r1= map(mouseY,100,400,50,150);
+  
    
-   // change the value of Green based on the mouse movement about the X axis
-  g= map(mouseX,600,400,90,100);
-
+  // change the value of Green based on the mouse movement about the X axis
+  g = map(mouseX,600,400,90,150);
+  g1= map(mouseX,600,400,90,150);
+ 
   // change the value of Blue based on the mouse movement about the X axis
-  b= map(mouseX,1200,400,100,150);
-
+  b = map(mouseX,1200,400,100,150);
+  b1= map(mouseX,1200,400,100,150);
   // Use the map() function to do so. 
 
   // Pass the values to the background() function you have learnt previously.
-  background(r,g,b);
+  background(r,r1,g,g1,b,b1);
 
+  
   // Remember to fill the canvas with white paint before creating the ellipse.
   fill(255);
 
